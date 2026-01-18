e324e9d875807ef945e25911b012b73fe72aba8f
anand-106 <anands2003106@gmail.com>
2026-01-18 21:19:57 +0530

    first commit

---
+++ apps/frontend/app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center ">
+
+      <h1>
+        Minutes AI
+      </h1>
+      
+      
+    </div>
+  );
+}
